22367a5f09786518cfa99d9f5fa7e723c1071241
Gabriele Tornatore <61290799+it9tst@users.noreply.github.com>
2021-04-17 12:57:33 +0200

    [CT-TEA] Client QAK

--- ct.tearoom/client.pl
+++ ct.tearoom/client.pl
@@ -1,3 +1,8 @@
 %====================================================================================
 % client description   
 %====================================================================================
+context(ctxtearoom, "localhost",  "TCP", "9901").
+context(ctxclient, "localhost",  "TCP", "9902").
+ qactor( smartbell, ctxtearoom, "external").
+  qactor( waiter, ctxtearoom, "external").
+  qactor( client, ctxclient, "it.unibo.client.Client").
