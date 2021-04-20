ea96449bd524c953b90620394de1e0d38d88c249
Gabriele Tornatore <61290799+it9tst@users.noreply.github.com>
2021-04-20 18:26:15 +0200

    [CT-TEA] Fix

--- ct.tearoom/barman.pl
+++ ct.tearoom/barman.pl
@@ -1,3 +1,6 @@
 %====================================================================================
 % barman description   
 %====================================================================================
+context(ctxtearoom, "localhost",  "TCP", "9901").
+ qactor( waiter, ctxtearoom, "external").
+  qactor( barman, ctxtearoom, "it.unibo.barman.Barman").
