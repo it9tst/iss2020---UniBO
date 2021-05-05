8325ec7bfd7c014a95804225fbc563ab366b2809
VittorioCorsale-1 <61838506+VittorioCorsale-1@users.noreply.github.com>
2021-05-05 18:25:44 +0200

    [CT-TEA] Add test folder and kt

---
+++ ct.tearoom/tests/test_01.kt
@@ -0,0 +1,19 @@
+package tests
+
+import org.junit.Before
+import org.junit.After
+import org.junit.Test
+import org.junit.Assert.assertTrue
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import it.unibo.kactor.ActorBasic
+import it.unibo.kactor.MsgUtil
+import it.unibo.kactor.MqttUtils
+import it.unibo.kactor.ApplMessage
+import it.unibo.kactor.ApplMessageType
+
+class Test01 {
+	
+}
